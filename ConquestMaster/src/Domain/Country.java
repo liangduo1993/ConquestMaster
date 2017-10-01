659d84a7f767f2f9a44b9dd46fa4f854f3d24976
mryanshenghong <mryanshenghong@gmail.com>
2017-10-02 03:48:52 +0800

    32123

--- ConquestMaster/src/Domain/Country.java
+++ ConquestMaster/src/Domain/Country.java
@@ -3,5 +3,17 @@ package Domain;
 public class Country {
 	public String name;
 	private int age;
+
+	public Country(String name, int age) {
+		// TODO Auto-generated constructor stub
+		this.name=name;
+		this.age=age;
+		
+	}
+	
+	public static void main(String[] args) {
+		Country c = new Country("alaska", 123);
+		System.out.println(c.name+" "+c.age);
+	}
 	
 }
