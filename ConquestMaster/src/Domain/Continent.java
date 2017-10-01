8e1a4663446da47e5bca2612e010f1a8dc0968e3
liangduo1993 <liangduo1993@gmail.com>
2017-10-01 16:28:37 -0400

    liang

---
+++ ConquestMaster/src/Domain/Continent.java
@@ -0,0 +1,37 @@
+package Domain;
+
+public class Continent {
+  String name;
+  int bonus;
+  
+  public Continent(String name, int bonus)
+  {
+    this.name = name;
+    this.bonus = bonus;
+  }
+  
+  public int getBonus()
+  {
+    return this.bonus;
+  }
+  
+  public String getName()
+  {
+    return this.name;
+  }
+  
+  public void setBonus(int bonus)
+  {
+    this.bonus = bonus;
+  }
+  
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  
+  public String toString()
+  {
+    return this.name;
+  }
+}
--- ConquestMaster/src/GameConsole/Game.java
+++ ConquestMaster/src/GameConsole/Game.java
@@ -3,6 +3,8 @@ package GameConsole;
 public class Game {
 
 
+	
+	
 
 	public static void main(String[] args) {
 
