c2aec3ad34caee244fba66537efd1fa35428326a
mryanshenghong <mryanshenghong@gmail.com>
2017-10-04 06:34:14 +0800

    NewMapFrame with absoluteLayout

--- ConquestMaster/src/MapEditor/View/NewMapFrame.java
+++ ConquestMaster/src/MapEditor/View/NewMapFrame.java
@@ -19,6 +19,18 @@ import javax.swing.SwingConstants;
 import MapEditor.mainFrame;
 import MapEditor.Core.ConquestMap;
 
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
 public class NewMapFrame {
 	// private ConquestMap map = mainFrame.map;
 	private JFrame frame;
@@ -37,26 +49,30 @@ public class NewMapFrame {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(400, 100, 450, 400);
+		frame.setBounds(400, 100, 338, 279);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JLabel lblNewLabel = new JLabel("Author:");
-
 		textField = new JTextField();
+		textField.setBounds(140, 5, 66, 21);
 		textField.setColumns(10);
 
 		wrapCheckBox = new JCheckBox("");
+		wrapCheckBox.setBounds(140, 52, 21, 21);
 		
 		warnCheckBox = new JCheckBox("");
+		warnCheckBox.setBounds(140, 91, 21, 21);
 
 		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(140, 145, 94, 21);
 		comboBox.setModel(new DefaultComboBoxModel(new String[] { "2", "3", "4", "5" }));
 
 		JLabel lblNewLabel_1 = new JLabel("ImagePath:");
+		lblNewLabel_1.setBounds(40, 217, 60, 15);
 
 		// ImagePath button no Name needed
 		 imgBtn = new JButton("Please select a image!");
+		 imgBtn.setBounds(140, 213, 165, 23);
 		imgBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -73,62 +89,28 @@ public class NewMapFrame {
 				}
 			});
 
-		JLabel lblNewLabel_2 = new JLabel("Wrap:");
-
-		JLabel lblNewLabel_3 = new JLabel("Warn:");
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-
 		JLabel lblNewLabel_4 = new JLabel("Scroll:");
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
-		groupLayout
-				.setHorizontalGroup(
-						groupLayout.createParallelGroup(Alignment.LEADING).addGroup(
-								groupLayout
-										.createSequentialGroup().addGroup(groupLayout.createParallelGroup(
-												Alignment.TRAILING)
-												.addGroup(groupLayout
-														.createSequentialGroup().addGap(55).addGroup(groupLayout
-																.createParallelGroup(Alignment.LEADING)
-																.addComponent(lblNewLabel).addComponent(lblNewLabel_1)
-																.addGroup(Alignment.TRAILING, groupLayout
-																		.createSequentialGroup()
-																		.addPreferredGap(ComponentPlacement.RELATED, 30,
-																				Short.MAX_VALUE)
-																		.addComponent(lblNewLabel_2).addGap(6))
-																.addComponent(lblNewLabel_4, Alignment.TRAILING))
-														.addGap(25))
-												.addGroup(groupLayout.createSequentialGroup().addContainerGap()
-														.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 49,
-																GroupLayout.PREFERRED_SIZE)
-														.addGap(18)))
-										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-												.addComponent(warnCheckBox).addComponent(wrapCheckBox)
-												.addComponent(textField, GroupLayout.PREFERRED_SIZE,
-														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-												.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-														.addComponent(imgBtn).addComponent(comboBox,
-																GroupLayout.PREFERRED_SIZE, 94,
-																GroupLayout.PREFERRED_SIZE)))
-										.addGap(201)));
-		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
-				.createSequentialGroup().addGap(5)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(lblNewLabel).addComponent(
-						textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addGap(26)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(wrapCheckBox)
-						.addComponent(lblNewLabel_2))
-				.addGap(18)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(warnCheckBox).addComponent(
-						lblNewLabel_3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-				.addGap(33)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-								GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_4))
-				.addGap(47).addGroup(groupLayout.createParallelGroup(Alignment.BASELINE, false)
-						.addComponent(lblNewLabel_1).addComponent(imgBtn))
-				.addGap(31)));
-		frame.getContentPane().setLayout(groupLayout);
+		lblNewLabel_4.setBounds(40, 148, 42, 15);
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().add(lblNewLabel_1);
+		frame.getContentPane().add(lblNewLabel_4);
+		frame.getContentPane().add(warnCheckBox);
+		frame.getContentPane().add(wrapCheckBox);
+		frame.getContentPane().add(textField);
+		frame.getContentPane().add(imgBtn);
+		frame.getContentPane().add(comboBox);
+		
+		JLabel lblNewLabel = new JLabel("Author:");
+		lblNewLabel.setBounds(40, 10, 54, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Warp:");
+		lblNewLabel_2.setBounds(40, 50, 54, 15);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Warn:");
+		lblNewLabel_3.setBounds(40, 90, 54, 15);
+		frame.getContentPane().add(lblNewLabel_3);
 
 		frame.setVisible(true);
 	}
