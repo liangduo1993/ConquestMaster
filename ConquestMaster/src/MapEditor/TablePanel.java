48f5175318af3ac8dd879d4ab3d304681a7c8ce5
mryanshenghong <mryanshenghong@gmail.com>
2017-10-04 06:24:27 +0800

    TablePanel

--- ConquestMaster/src/MapEditor/TablePanel.java
+++ ConquestMaster/src/MapEditor/TablePanel.java
@@ -9,11 +9,14 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JButton;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TablePanel {
 
 	private JFrame frame;
 	private JTable table;
+	private JTable table_1;
 	/**
 	 * @wbp.nonvisual location=337,417
 	 */
@@ -118,26 +121,101 @@ public class TablePanel {
 		scrollPane_2.setBounds(183, 10, 163, 460);
 		panel.add(scrollPane_2);
 		
+		table_1 = new JTable();
+		table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+			},
+			new String[] {
+				"Continent"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		scrollPane_2.setViewportView(table_1);
+		
+		//Territory
 		JButton btnNewButton = new JButton("Add");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton.setBounds(46, 480, 93, 23);
 		panel.add(btnNewButton);
 		
+		//Territory
 		JButton btnNewButton_1 = new JButton("Edit");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton_1.setBounds(46, 513, 93, 23);
 		panel.add(btnNewButton_1);
 		
+		//Territory
 		JButton btnNewButton_2 = new JButton("Delete");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton_2.setBounds(46, 546, 93, 23);
 		panel.add(btnNewButton_2);
 		
+		//Continent
 		JButton btnAdd = new JButton("Add");
 		btnAdd.setBounds(218, 480, 93, 23);
 		panel.add(btnAdd);
 		
+		//Continent
 		JButton btnEdit = new JButton("Edit");
 		btnEdit.setBounds(218, 513, 93, 23);
 		panel.add(btnEdit);
 		
+		//Continent
 		JButton btnDelete = new JButton("Delete");
 		btnDelete.setBounds(218, 546, 93, 23);
 		panel.add(btnDelete);
