f47e29c6f4b5ae416af88f4052cd068c3868f4bc
liangduo1993 <liangduo1993@gmail.com>
2017-10-16 07:55:11 -0400

    liang

--- ConquestMaster/test/MapEditor/MapEditSuiteTest.java
+++ ConquestMaster/test/MapEditor/MapEditSuiteTest.java
@@ -5,11 +5,13 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class MapEditSuiteTest {
-public static void main(String[] args) {
-	Result result = JUnitCore.runClasses(SuiteTest.class);
-	for(Failure failure: result.getFailures()){
-		System.out.println(failure.toString());
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(SuiteTest.class);
+		System.out.println("//=====================");
+		for (Failure failure : result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+		System.out.println(result.wasSuccessful());
+		System.out.println("//=====================");
 	}
-	System.out.println(result.wasSuccessful());
-}
 }
