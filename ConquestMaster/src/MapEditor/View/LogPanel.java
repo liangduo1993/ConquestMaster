67fa6468d706ccd2e0519338a7ead9c8dc7182cc
mryanshenghong <mryanshenghong@gmail.com>
2017-10-16 12:29:55 +0800

    logpanel comments

--- ConquestMaster/src/MapEditor/View/LogPanel.java
+++ ConquestMaster/src/MapEditor/View/LogPanel.java
@@ -6,11 +6,16 @@ import java.awt.Insets;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-
+/**
+ * this class is the GUI for the logpanel, to show the any message when editing a conquest map. 
+ */
 public class LogPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	public JTextArea log = new JTextArea();
 
+	/**
+	 * constructor method, setting the logPanel location.
+	 */
 	public LogPanel() {
 		super(new BorderLayout());
 
