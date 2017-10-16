b8c3c28d7bc2dc9d1cccc1077ea0223261ef5b11
JingYang0521 <jingy0521@gmail.com>
2017-10-16 15:38:04 -0400

    jing
    comment

--- ConquestMaster/src/GameConsole/Player/Group.java
+++ ConquestMaster/src/GameConsole/Player/Group.java
@@ -5,24 +5,48 @@ import java.util.ArrayList;
 import GameConsole.World.World;
 
 
+/**
+ * This class is to store all the players into a list and manage to add players, set and
+ * get the world.
+ */
 public class Group {
 	private ArrayList<Player> players = new ArrayList<Player>();
 	private World world;
 	private ArrayList<Player> place = new ArrayList<Player>();
 	private String groupName;
-	
+
+	/**
+	 * To get the players list
+	 * @return the list contains the players
+	 */
 	public ArrayList<Player> getPlayers() {
 		return players;
 	}
+
 	public void setPlayers(ArrayList<Player> players) {
 		this.players = players;
 	}
+
+	/**
+	 * To add a player to the players list
+	 * @param p1 the player need to be added
+	 */
 	public void addPlayer(Player p1) {
 		this.players.add(p1);
 	}
+
+	/**
+	 * To get the world where the players can play in
+	 * @return the game's world
+	 */
 	public World getWorld() {
 		return world;
 	}
+
+	/**
+	 * To set the desired world
+	 * @param world the world wanted to be setted
+	 */
 	public void setWorld(World world) {
 		this.world = world;
 	}
@@ -38,6 +62,5 @@ public class Group {
 	public void setGroupName(String groupName) {
 		this.groupName = groupName;
 	}
-	
-	
+
 }
