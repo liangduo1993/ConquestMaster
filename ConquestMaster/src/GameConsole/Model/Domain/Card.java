14e3e6e1100ccf0b274dca18f82ca75a1fc98f7b
liangduo1993 <liangduo1993@gmail.com>
2017-10-18 08:09:53 -0400

    liang

--- ConquestMaster/src/GameConsole/Model/Domain/Card.java
+++ ConquestMaster/src/GameConsole/Model/Domain/Card.java
@@ -6,45 +6,49 @@ import GameConsole.Model.Player.Player;
  * This class defines the Cards that player can exchange to troops
  *
  */
-public class Card{
+@SuppressWarnings("all")
+public class Card {
 
-	   private int type; // 0, 1, 2 three types
-	   private Player player;
-	   
-	   public Card(Player player){
-	      this.player = player;
-	   }
-	   
-	   public String toString(){
-		return "This is a "+type+" card";
-		   
-	   }
-	   
-	   /**
-	    * If player conquered at least one territory during the attack phase, he will be given a card
-	    * 
-	    */
-	   public void addRandomTypeCard(){
-		   this.type=(int) (Math.random()*3);
-	   }
-	   
+	private int type; // 0, 1, 2 three types
+	private Player player;
 
-	   /**
-	    * Gives all of the killed Player's cards to their killer
-	    * @param giver Player who was killed
-	    * @param receiver Player who killed giver
-	    */
-	   public void handOverCards(Player giver, Player receiver){
-	         receiver.addAllCard(giver.getOnHand());
-	   }
+	public Card(Player player) {
+		this.player = player;
+	}
 
-		/**
-		 * To get the type of the hand card
-		 * @return the type of the hand card with int type
-		 */
-	   public int getType()
-	   {
-	      return type;
-	   }
-	   
+	public String toString() {
+		return "This is a " + type + " card";
+
+	}
+
+	/**
+	 * If player conquered at least one territory during the attack phase, he
+	 * will be given a card
+	 * 
+	 */
+	public void addRandomTypeCard() {
+		this.type = (int) (Math.random() * 3);
+	}
+
+	/**
+	 * Gives all of the killed Player's cards to their killer
+	 * 
+	 * @param giver
+	 *            Player who was killed
+	 * @param receiver
+	 *            Player who killed giver
+	 */
+	public void handOverCards(Player giver, Player receiver) {
+		receiver.addAllCard(giver.getOnHand());
+	}
+
+	/**
+	 * To get the type of the hand card
+	 * 
+	 * @return the type of the hand card with int type
+	 */
+	public int getType() {
+		return type;
+	}
+
 }
