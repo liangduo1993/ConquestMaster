939247b9059bc874f4d592e912893b6e85179ecc
mryanshenghong <mryanshenghong@gmail.com>
2017-10-28 02:17:09 +0800

    DominationInformationPanel

---
+++ ConquestMaster/src/GameConsole/View/DomiInfoPanel.java
@@ -0,0 +1,143 @@
+package GameConsole.View;
+
+import java.awt.EventQueue;
+import java.awt.ScrollPane;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+
+public class DomiInfoPanel {
+
+	private JFrame frame;
+
+	private JTable table_11;
+	private JTable table_12;
+	private JTable table_13;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DomiInfoPanel window = new DomiInfoPanel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public DomiInfoPanel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 346, 533);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("player1");
+		lblNewLabel.setBounds(36, 55, 54, 15);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("player2");
+		lblNewLabel_1.setBounds(36, 214, 54, 15);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("player3");
+		lblNewLabel_2.setBounds(36, 387, 54, 15);
+		panel.add(lblNewLabel_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(157, 26, 149, 76);
+		panel.add(scrollPane);
+		
+		table_11 = new JTable();
+		table_11.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"New column", "New column"
+			}
+		));
+		scrollPane.setViewportView(table_11);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(157, 191, 149, 83);
+		panel.add(scrollPane_1);
+		
+		table_12 = new JTable();
+		table_12.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"New column", "New column"
+			}
+		));
+		scrollPane_1.setViewportView(table_12);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(157, 369, 149, 83);
+		panel.add(scrollPane_2);
+		
+		table_13 = new JTable();
+		table_13.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"New column", "New column"
+			}
+		));
+		scrollPane_2.setViewportView(table_13);
+
+		
+
+
+}
+}
