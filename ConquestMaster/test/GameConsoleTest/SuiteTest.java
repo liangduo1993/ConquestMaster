3c4fd14d3cb01867e2ca444c0812bc35988fc16b
lzlurker <31750946+lzlurker@users.noreply.github.com>
2017-11-07 19:07:22 -0500

    Clean the empty test cases

--- ConquestMaster/test/GameConsoleTest/SuiteTest.java
+++ ConquestMaster/test/GameConsoleTest/SuiteTest.java
@@ -4,18 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import MapEditorTest.ConquestMapTest;
-import MapEditorTest.ContinentTest;
-import MapEditorTest.FileChooserTest;
-import MapEditorTest.InputContinentFrameTest;
-import MapEditorTest.InputTerritoryFrameTest;
-import MapEditorTest.LogPanelTest;
-import MapEditorTest.MainFrameTest;
 import MapEditorTest.MyStringUtilTest;
-import MapEditorTest.NewMapFrameTest;
-import MapEditorTest.SettingsFrameTest;
-import MapEditorTest.StringUtilTest;
 import MapEditorTest.TablePanelTest;
-import MapEditorTest.TerritoryTest;
+
 
 
 /**
@@ -31,31 +22,9 @@ import MapEditorTest.TerritoryTest;
 		GameStateTest.class,//
 		CardsTest.class,//
 		MapLoaderTest.class,//
-		DriverTest.class,//
-		WorldTest.class,//
-		AbstractTroopTest.class,//
-		TroopTest.class,//
-		ContinentTest.class,//
-		CountryDecoratorTest.class,//
-		ConquestRatioTest.class,//
-		CountryButtonTest.class,//
-		DomiInfoPanelTest.class,//
-		LogPanelTest.class,//
-		MapDisplayerTest.class,//
-		WindowMainTest.class,//
 		ConquestMapTest.class, //
 		MyStringUtilTest.class, //
 		TablePanelTest.class,//
-		ContinentTest.class,//
-		FileChooserTest.class,//
-		InputContinentFrameTest.class,//
-		InputTerritoryFrameTest.class,//
-		LogPanelTest.class,//
-		MainFrameTest.class,//
-		NewMapFrameTest.class,//
-		SettingsFrameTest.class,//
-		StringUtilTest.class,//
-		TerritoryTest.class//
 		
 })
 public class SuiteTest {
