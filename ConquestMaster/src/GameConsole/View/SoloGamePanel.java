9e5afc5ed8d518ecc18c3194ac2e4145421cbf1c
mryanshenghong <mryanshenghong@gmail.com>
2017-11-16 05:08:18 +0800

    Tournament Mode----sologamepanel

--- ConquestMaster/src/GameConsole/View/SoloGamePanel.java
+++ ConquestMaster/src/GameConsole/View/SoloGamePanel.java
@@ -6,17 +6,41 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Color;
 import java.awt.Font;
+import java.io.File;
+
 import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.UIManager;
 
+import GameConsole.Core.GameState;
+
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class SoloGamePanel {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JFileChooser fc1;
+	private JFileChooser fc2;
+	private JFileChooser fc3;
+	private JFileChooser fc4;
+	private JFileChooser fc5;
+	
+	private JLabel M1Label;
+	private JLabel M2Label;
+	private JLabel M3Label;
+	private JLabel M4Label;
+	private JLabel M5Label;
+	
 
 	/**
 	 * Launch the application.
@@ -49,116 +73,221 @@ public class SoloGamePanel {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 751, 630);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setBounds(0, 0, 735, 591);
+		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("Player Selection ");
 		lblNewLabel.setBackground(new Color(255, 255, 0));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 29));
+		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 22));
 		lblNewLabel.setForeground(Color.DARK_GRAY);
-		lblNewLabel.setBounds(23, 21, 702, 66);
+		lblNewLabel.setBounds(0, 0, 182, 38);
 		panel.add(lblNewLabel);
 
-		JLabel p1Label = new JLabel("Player 1:");
-		p1Label.setBounds(91, 110, 54, 15);
+		JLabel p1Label = new JLabel("Computer 1:");
+		p1Label.setBounds(91, 76, 75, 15);
 		panel.add(p1Label);
 
-		JLabel p2Label = new JLabel("Player 2:");
-		p2Label.setBounds(91, 160, 54, 15);
+		JLabel p2Label = new JLabel("Computer 2:");
+		p2Label.setBounds(381, 76, 75, 15);
 		panel.add(p2Label);
 
-		JLabel p3Label = new JLabel("Player 3:");
-		p3Label.setBounds(91, 210, 54, 15);
+		JLabel p3Label = new JLabel("Computer 3:");
+		p3Label.setBounds(91, 126, 75, 15);
 		panel.add(p3Label);
 
-		JLabel p4Label = new JLabel("Player 4:");
-		p4Label.setBounds(91, 260, 54, 15);
+		JLabel p4Label = new JLabel("Computer 4:");
+		p4Label.setBounds(381, 123, 75, 15);
 		panel.add(p4Label);
-
-		JLabel p5Label = new JLabel("Player 5:");
-		p5Label.setBounds(91, 310, 54, 15);
-		panel.add(p5Label);
-
-		JLabel p6Label = new JLabel("Player 6:");
-		p6Label.setBounds(91, 360, 54, 15);
-		panel.add(p6Label);
-
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] { "none", "human", "computer" }));
-		comboBox.setBounds(244, 110, 102, 21);
-		panel.add(comboBox);
-
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] { "none", "human", "computer" }));
-		comboBox_1.setBounds(244, 160, 102, 21);
-		panel.add(comboBox_1);
-
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] { "none", "human", "computer" }));
-		comboBox_2.setBounds(244, 210, 102, 21);
-		panel.add(comboBox_2);
-
-		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setModel(new DefaultComboBoxModel(new String[] { "none", "human", "computer" }));
-		comboBox_3.setBounds(244, 260, 102, 21);
-		panel.add(comboBox_3);
-
-		JComboBox comboBox_4 = new JComboBox();
-		comboBox_4.setModel(new DefaultComboBoxModel(new String[] { "none", "human", "computer" }));
-		comboBox_4.setBounds(244, 310, 102, 21);
-		panel.add(comboBox_4);
-
-		JComboBox comboBox_5 = new JComboBox();
-		comboBox_5.setModel(new DefaultComboBoxModel(new String[] { "none", "human", "computer" }));
-		comboBox_5.setBounds(244, 360, 102, 21);
-		panel.add(comboBox_5);
 
 		JComboBox comboBox_6 = new JComboBox();
 		comboBox_6.setModel(
 				new DefaultComboBoxModel(new String[] { "aggressive ", "benevolent ", "random ", "cheater " }));
-		comboBox_6.setBounds(500, 107, 102, 21);
+		comboBox_6.setBounds(211, 76, 102, 21);
 		panel.add(comboBox_6);
 
 		JComboBox comboBox_7 = new JComboBox();
 		comboBox_7.setModel(
 				new DefaultComboBoxModel(new String[] { "aggressive ", "benevolent ", "random ", "cheater " }));
-		comboBox_7.setBounds(500, 157, 102, 21);
+		comboBox_7.setBounds(501, 76, 102, 21);
 		panel.add(comboBox_7);
 
 		JComboBox comboBox_8 = new JComboBox();
 		comboBox_8.setModel(
 				new DefaultComboBoxModel(new String[] { "aggressive ", "benevolent ", "random ", "cheater " }));
-		comboBox_8.setBounds(500, 207, 102, 21);
+		comboBox_8.setBounds(211, 126, 102, 21);
 		panel.add(comboBox_8);
 
 		JComboBox comboBox_9 = new JComboBox();
 		comboBox_9.setModel(
 				new DefaultComboBoxModel(new String[] { "aggressive ", "benevolent ", "random ", "cheater " }));
-		comboBox_9.setBounds(500, 260, 102, 21);
+		comboBox_9.setBounds(501, 126, 102, 21);
 		panel.add(comboBox_9);
-
-		JComboBox comboBox_10 = new JComboBox();
-		comboBox_10.setModel(
-				new DefaultComboBoxModel(new String[] { "aggressive ", "benevolent ", "random ", "cheater " }));
-		comboBox_10.setBounds(500, 307, 102, 21);
-		panel.add(comboBox_10);
-
-		JComboBox comboBox_11 = new JComboBox();
-		comboBox_11.setModel(
-				new DefaultComboBoxModel(new String[] { "aggressive ", "benevolent ", "random ", "cheater " }));
-		comboBox_11.setBounds(500, 357, 102, 21);
-		panel.add(comboBox_11);
 
 		JButton startGameButt = new JButton("Start Game");
 		startGameButt.setBackground(Color.WHITE);
-		startGameButt.setBounds(198, 443, 102, 23);
+		startGameButt.setBounds(175, 537, 102, 23);
 		panel.add(startGameButt);
 
 		JButton CancelButt = new JButton("Cancel");
-		CancelButt.setBounds(447, 443, 93, 23);
+		CancelButt.setBounds(448, 537, 93, 23);
 		panel.add(CancelButt);
+		
+		JLabel lblNewLabel_1 = new JLabel("Games Number");
+		lblNewLabel_1.setBounds(91, 185, 75, 15);
+		panel.add(lblNewLabel_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5"}));
+		comboBox.setBounds(211, 182, 102, 21);
+		panel.add(comboBox);
+		
+		JLabel lblNewLabel_2 = new JLabel("Max Turns :");
+		lblNewLabel_2.setBounds(381, 185, 75, 15);
+		panel.add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(501, 182, 102, 21);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewM5Label = new JLabel("(input 10-50)");
+		lblNewM5Label.setBounds(628, 185, 84, 15);
+		panel.add(lblNewM5Label);
+		
+		JLabel lblNewLabel_4 = new JLabel("Map Selection");
+		lblNewLabel_4.setFont(new Font("Calibri", Font.PLAIN, 22));
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.setBounds(0, 220, 182, 38);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Map 1:");
+		lblNewLabel_5.setBounds(91, 260, 54, 15);
+		panel.add(lblNewLabel_5);
+		
+		JLabel lblMap = new JLabel("Map 2:");
+		lblMap.setBounds(91, 300, 54, 15);
+		panel.add(lblMap);
+		
+		JLabel lblMap_1 = new JLabel("Map 3:");
+		lblMap_1.setBounds(91, 340, 54, 15);
+		panel.add(lblMap_1);
+		
+		JLabel lblMap_2 = new JLabel("Map 4:");
+		lblMap_2.setBounds(91, 380, 54, 15);
+		panel.add(lblMap_2);
+		
+		JLabel lblMap_3 = new JLabel("Map 5:");
+		lblMap_3.setBounds(91, 420, 54, 15);
+		panel.add(lblMap_3);
+		
+		JButton btnSelectM = new JButton("select M1");
+		btnSelectM.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fc1 = new JFileChooser();
+				int returnVal = fc1.showOpenDialog(null);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc1.getSelectedFile();
+					String path = file.getAbsolutePath();
+					M1Label.setText(path);
+			}
+			}
+		});
+		btnSelectM.setBounds(211, 256, 93, 23);
+		panel.add(btnSelectM);
+		
+		JButton btnSelectM_1 = new JButton("select M2");
+		btnSelectM_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fc2 = new JFileChooser();
+				int returnVal = fc2.showOpenDialog(null);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc2.getSelectedFile();
+					String path = file.getAbsolutePath();
+					M1Label.setText(path);
+			}
+			}
+		});
+		btnSelectM_1.setBounds(211, 296, 93, 23);
+		panel.add(btnSelectM_1);
+		
+		JButton btnSelectM_2 = new JButton("select M3");
+		btnSelectM_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fc3 = new JFileChooser();
+				int returnVal = fc3.showOpenDialog(null);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc3.getSelectedFile();
+					String path = file.getAbsolutePath();
+					M1Label.setText(path);
+			}
+			}
+		});
+		btnSelectM_2.setBounds(211, 336, 93, 23);
+		panel.add(btnSelectM_2);
+		
+		JButton btnSelectM_3 = new JButton("select M4");
+		btnSelectM_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fc4 = new JFileChooser();
+				int returnVal = fc4.showOpenDialog(null);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc4.getSelectedFile();
+					String path = file.getAbsolutePath();
+					M1Label.setText(path);
+				}
+			}
+		});
+		btnSelectM_3.setBounds(211, 376, 93, 23);
+		panel.add(btnSelectM_3);
+		
+		JButton btnSelectM_4 = new JButton("select M5");
+		btnSelectM_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fc5 = new JFileChooser();
+				int returnVal = fc5.showOpenDialog(null);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc5.getSelectedFile();
+					String path = file.getAbsolutePath();
+					M1Label.setText(path);
+				}
+			}
+		});
+		btnSelectM_4.setBounds(211, 416, 93, 23);
+		panel.add(btnSelectM_4);
+		
+		M1Label = new JLabel("");
+		M1Label.setHorizontalAlignment(SwingConstants.CENTER);
+		M1Label.setForeground(Color.RED);
+		M1Label.setBounds(381, 260, 297, 19);
+		panel.add(M1Label);
+		
+		M2Label = new JLabel("");
+		M2Label.setHorizontalAlignment(SwingConstants.CENTER);
+		M2Label.setForeground(Color.RED);
+		M2Label.setBounds(381, 298, 297, 19);
+		panel.add(M2Label);
+		
+		M3Label= new JLabel("");
+		M3Label.setHorizontalAlignment(SwingConstants.CENTER);
+		M3Label.setForeground(Color.RED);
+		M3Label.setBounds(381, 340, 297, 19);
+		panel.add(M3Label);
+		
+		M4Label= new JLabel("");
+		M4Label.setHorizontalAlignment(SwingConstants.CENTER);
+		M4Label.setForeground(Color.RED);
+		M4Label.setBounds(381, 380, 297, 19);
+		panel.add(M4Label);
+		
+		M5Label = new JLabel("");
+		M5Label.setHorizontalAlignment(SwingConstants.CENTER);
+		M5Label.setForeground(Color.RED);
+		M5Label.setBounds(381, 420, 297, 19);
+		panel.add(M5Label);
 	}
 }
