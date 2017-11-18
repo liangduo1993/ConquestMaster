1904e6bb114d681fee8b2b9c4116a7562ae7e562
liangduo1993 <liangduo1993@gmail.com>
2017-11-18 13:43:26 -0500

    liang

--- ConquestMaster/src/GameConsole/Core/GameStimulater.java
+++ ConquestMaster/src/GameConsole/Core/GameStimulater.java
@@ -52,51 +52,51 @@ public class GameStimulater {
 				System.out.println(p.getName() + ":" + p.getCountries().size());
 			}
 			this.players.removeAll(removeList);
-			
+
 			System.out.println(gs.getFirstRound());
-			
+
 			if (players.size() == 1) {
 				return players.get(0).getStrategy().getName();
 			}
 
-			
-			if(currPlayer == players.get(players.size() - 1)){
+			if (currPlayer == players.get(players.size() - 1)) {
 				gs.setCurrPlayer(players.get(0));
 				gs.setFirstRound(gs.getFirstRound() + 1);
-			}else{
+			} else {
 				gs.setCurrPlayer(players.get(players.indexOf(currPlayer) + 1));
 			}
-			
-			
 
 		}
 
 		return "draw";
 	}
 
-	
-	//AggressiveStrategy
-	//BenevolentStrategy
-	//CheaterStrategy
-	//RandomStrategy
+	// AggressiveStrategy
+	// BenevolentStrategy
+	// CheaterStrategy
+	// RandomStrategy
 	public static void main(String[] args) throws Exception {
-		GameState gs = new GameState(null, "C:\\Users\\Liang\\Documents\\Atlantis.map");
-		Player p1 = new Player("p1", Color.magenta, gs, new AggressiveStrategy());
-		Player p2 = new Player("p2", Color.green, gs, new BenevolentStrategy());
-		Player p3 = new Player("p3", Color.blue, gs, new CheaterStrategy());
-		Player p4 = new Player("p4", Color.red, gs, new RandomStrategy());
-		List<Player> list = new ArrayList<>();
-		list.add(p1);
-		list.add(p2);
-		list.add(p3);
-		list.add(p4);
-		
-		
-		GameStimulater gameSt = new GameStimulater(gs, list, 5);
-		gs.gameStart(false);
+		StringBuffer sb = new StringBuffer(100);
+		for (int i = 0; i < 50; i++) {
+			GameState gs = new GameState(null, "C:\\Users\\Liang\\Documents\\Atlantis.map");
+			Player p1 = new Player("p1", Color.magenta, gs, new AggressiveStrategy());
+			Player p2 = new Player("p2", Color.green, gs, new BenevolentStrategy());
+			//Player p3 = new Player("p3", Color.blue, gs, new CheaterStrategy());
+			Player p4 = new Player("p4", Color.red, gs, new RandomStrategy());
+			List<Player> list = new ArrayList<>();
+			list.add(p1);
+			list.add(p2);
+			//list.add(p3);
+			list.add(p4);
 
+			GameStimulater gameSt = new GameStimulater(gs, list, 30);
+			gs.gameStart(false);
+			sb.append(gameSt.execute());
+			sb.append("\r\n");
+
+		}
 		System.out.println("=====================");
-		System.out.println(gameSt.execute());
+		System.out.println(sb.toString());
 		System.out.println("=====================");
 
 	}
